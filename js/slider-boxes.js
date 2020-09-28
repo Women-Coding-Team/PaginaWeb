0371ca7427ca8b73220b29c5da7b41eadfff735b
MooiBlack <mooimosho@hotmail.com>
2020-09-27 23:26:55 -0500

    Se han agregado dos carruseles con tarjetas en las secciones de proyectos y noticias

---
+++ js/slider-boxes.js
@@ -0,0 +1,21 @@
+$('#recipeCarousel').carousel({
+  interval: 10000
+})
+
+$('.carrusel .carrusel-item').each(function(){
+    var minPerSlide = 2;
+    var next = $(this).next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+    
+    for (var i=0;i<minPerSlide;i++) {
+        next=next.next();
+        if (!next.length) {
+        	next = $(this).siblings(':first');
+      	}
+        
+        next.children(':first-child').clone().appendTo($(this));
+      }
+});
